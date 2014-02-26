334bf1a4343b7ae134db7641182e3171b9d6a6d1
michoello <midolini@midolini.redmond.corp.microsoft.com>
2014-02-25 16:39:56 -0800

    more and more lisp

--- sc.rb
+++ sc.rb
@@ -13,25 +13,24 @@ Rambler = Struct.new(:todo, :done, :cost, :road, :chance) do
    end 
 end
 
-def rambiter(r)
-   return r if r.road.t.keys.empty? && r.todo.empty?
-
-   team = r.road.t.map { |dst, road| Rambler.new(r.todo, r.done+dst, r.cost+1, road) }         # insert
-   
-   unless r.todo.empty?
-      team += r.road.t.map { |dst, road| Rambler.new(r.todo[1..-1], r.done+dst, r.cost+1, road)}          # replace 
-      team << Rambler.new(r.todo[1..-1], r.done, r.cost+1, r.road)  # delete   
-
-      while r.road.t.has_key?(r.todo[0])
-         team << (r = Rambler.new(r.todo[1..-1], r.done+r.todo[0], r.cost, r.road.t[r.todo[0]]))     # staight or finish
-      end
-   end
-   team
-end
-
 def spellcheck(word, max_cost, team_size)
-   (onestep = lambda do |walkers|; 
-      stepfwd = walkers.map{|r| rambiter(r)}.flatten
+   (onestep = lambda do |walkers| 
+      stepfwd = walkers.map do |r| 
+         team = [r] 
+         unless r.road.t.keys.empty? && r.todo.empty?
+            team = r.road.t.map { |dst, road| Rambler.new(r.todo, r.done+dst, r.cost+1, road) }           # insert
+            unless r.todo.empty?
+               team += r.road.t.map { |dst, road| Rambler.new(r.todo[1..-1], r.done+dst, r.cost+1, road)} # replace 
+               team << Rambler.new(r.todo[1..-1], r.done, r.cost+1, r.road)                               # delete   
+
+               while r.road.t.has_key?(r.todo[0])
+                  team << (r = Rambler.new(r.todo[1..-1], r.done+r.todo[0], r.cost, r.road.t[r.todo[0]])) # straight as far as possible
+               end
+            end
+         end
+         team
+      end .flatten
+
       stepfwd.size == walkers.size ? 
          walkers : onestep.call( stepfwd.select{|r| r.cost <= max_cost}.sort{|a, b| b.chance <=> a.chance}.uniq{|r| r.done+r.todo}[0..team_size] ) 
    end)
