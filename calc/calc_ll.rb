b7b76fd634ee06a222a0ad9daf1f91a154a6906c
michoello <midolini@midolini.redmond.corp.microsoft.com>
2014-03-06 14:41:39 -0800

    'tmp'

--- calc/calc_ll.rb
+++ calc/calc_ll.rb
@@ -70,52 +70,52 @@ $i = 0
 $RULEST
 
 def ParseLL(stack, tokens, i=0)
-   print "#{$i} WE ARE CALLED WITH STACK: #{stack}\n"; s = gets; exit if s == "q\n"
+#   print "#{$i} WE ARE CALLED WITH STACK: #{stack}\n"; s = gets; exit if s == "q\n"
 
-   if 1 then
+#   if 1 then
       stackorig, iorig = stack.clone, i
 
       rules = $grammar.select {|rule| (rule[1] == stack[-1]) && start_terms([ rule[0][0] ]).include?(tokens[i].value) }.reverse
-      print "WE HAVE ON TOP NOW #{stack[-1]}, and possible rules are ", rules.map{|rule| "[" + rule[0].to_s + "]"}.join(", "), "\n"
+#      print "WE HAVE ON TOP NOW #{stack[-1]}, and possible rules are ", rules.map{|rule| "[" + rule[0].to_s + "]"}.join(", "), "\n"
 
       rules.each do |rule|
-         print "#{i}: NOW RULE: [ #{rule[0]} #{rule[1]} ]\nSTACK WAS: #{stack}\n"
+       #  print "#{i}: NOW RULE: [ #{rule[0]} #{rule[1]} ]\nSTACK WAS: #{stack}\n"
          stack.pop 
          stack.push( *rule[0].reverse )
-         print "#{i}: STACK NOW: #{stack}\n"
+      #   print "#{i}: STACK NOW: #{stack}\n"
 
          while ( !stack.empty? ) do
-            print "#{i} CALL WITH NEXT ELT OF STACK #{stack}\n"
+     #       print "#{i} CALL WITH NEXT ELT OF STACK #{stack}\n"
 
             if is_term(stack[-1]) 
             then
-               print "#{i}: TERM FOUND: #{tokens[i].value}\n"
+    #           print "#{i}: TERM FOUND: #{tokens[i].value}\n"
                if ( stack[-1] == tokens[i].value ) then
                   stack.pop
                   print "#{i}: ---------------------------------------------- TERM IS OK, moving forward #{tokens[i].value}\n"
                   i = i + 1
                else
-                  print "#{i} TERM IS WRONG, EXPECTED [#{stack[-1]}]\n"
+   #               print "#{i} TERM IS WRONG, EXPECTED [#{stack[-1]}]\n"
                   break
                end
             else
                i = ParseLL( stack, tokens, i)
             end
 
-            print "[#{i}]: STACK OOO: #{stack}\n"
+  #          print "[#{i}]: STACK OOO: #{stack}\n"
             if ( i == tokens.size ) then
                print "FINITA: [#{rule[0]}] -> [#{rule[1]}]\n"
                return i
             end
         
          end
-         print "TRY NEXT RULE\n"
+ #        print "TRY NEXT RULE\n"
 
          stack, i = stackorig.clone, iorig
       end
-      print "NO MORE RULES AT THIS LEVEL\n"
-   end
-   print "LEVEL UP: #{i}!\n"
+#      print "NO MORE RULES AT THIS LEVEL\n"
+#   end
+#   print "LEVEL UP: #{i}!\n"
    return i
 end
 
