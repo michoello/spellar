34273a59a1da44f082047dba65d767eff14a0dcc
michoello <midolini@midolini.redmond.corp.microsoft.com>
2014-03-24 16:43:57 -0700

    working version

--- diff/diff.rb
+++ diff/diff.rb
@@ -1,42 +1,47 @@
-a = [2,3,4,1,2,3,4,5,6,7,8]
-b = [3,4,5,6,8,9]
+require 'digest/md5'
 
-print a, b, "\n"
-
-
+$cache = {}
 
 class Diff
    def self.diff_impl(a,b)
+     
+      key = Digest::MD5.hexdigest(a.to_s + " " + b.to_s) 
+      return $cache[key].clone if $cache.has_key? key
+
       as, bs = a.size, b.size
-      return [a.size, a.map{|x| "-"+x}] if bs == 0
-      return [b.size, b.map{|x| "+"+x}] if as == 0
 
-      plus = a[0] == b[0] ? 0 : 1
-  
-      c = [] 
-      c[0] = diff(a[1..as-1], b[1..bs-1])
-      c[1] = diff(a[0..as-1], b[1..bs-1])
-      c[2] = diff(a[1..as-1], b[0..bs-1])
-      c[0][0] += plus*2
-      c[1][0] += 1
-      c[2][0] += 1
+      r = []
+      r = [a.size, a.map{|x| "-"+x}] if bs == 0
+      r = [b.size, b.map{|x| "+"+x}] if as == 0
+
+      if r.empty?
+         plus = a[0] == b[0] ? 0 : 2
+
+         c = [ diff_impl(a[1..as-1], b[1..bs-1]),
+               diff_impl(a[0..as-1], b[1..bs-1]),
+               diff_impl(a[1..as-1], b[0..bs-1]) ]
+         c[0][0] += plus 
+         c[1][0] += 1
+         c[2][0] += 1
  
-      c[0][1] = (plus==0 ? [a[0]] : [a[0] + '/' + b[0]]) + c[0][1]
-      c[1][1] = ["+"+b[0]] + c[1][1]
-      c[2][1] = ["-"+a[0]] + c[2][1]
 
-      r = 0
+         c[0][1] = (plus==0 ? [a[0]] : [a[0] + '/' + b[0]]) + c[0][1]
+         c[1][1] = ["+"+b[0]] + c[1][1]
+         c[2][1] = ["-"+a[0]] + c[2][1]
 
-      a01 = c[0][0] <= c[1][0]
-      a02 = c[0][0] <= c[2][0]
-      a12 = c[1][0] <= c[2][0]
+         a01 = c[0][0] <= c[1][0]
 
-      return c[0] if a01 && a02
-      return c[1] if !a01 && a12
-      return c[2] 
+         r = c[2] 
+         r = c[0] if  a01 && (c[0][0] <= c[2][0])
+         r = c[1] if !a01 && (c[1][0] <= c[2][0])
+      end
+
+      $cache[key] = r.clone
+      r
    end
 
    def self.diff(a,b)
+      $cache.clear
       diff_impl a.map(&:to_s), b.map(&:to_s)
    end
 
@@ -44,4 +49,12 @@ end
 
 
 
-print Diff.diff([1,2,3,4,5], [2,3,5,6])
+a, b = [1,2,3,4,5,7,8,9,1,2,3,4,5,6,7,8,9], [2,3,5,6,7,1,9,4,5,6,6,7,8]
+#a,b = [1,2,3,4,5], [2,3,5,6]
+print a, "\n", b, "\n"
+
+
+print Diff.diff(a,b), "\n"
+
+
+
