5c5660b73f67bf33b00c9ac41ee7fb332734e9e5
michoello <midolini@midolini.redmond.corp.microsoft.com>
2014-03-25 18:00:56 -0700

    'bzz'

--- diff/diff.rb
+++ diff/diff.rb
@@ -12,23 +12,33 @@ end
 
 # w is amount of lines surrounding modified parts, a and b are compared arrays
 def diff(w,a,b)
-   rs = [ Cand.new(0, a, b, []) ]
+   as, bs = a.size, b.size
+   rs = [ Cand.new(0, 0, 0, []) ]
 
    until rs.empty? do
       rs = rs.map do |r|
-         price, aa, bb, diff = r.values
-         as, bs = aa.size, bb.size
+         price, ai, bi, diff = r.values
+#         as, bs = aa.size, bb.size
 
-         if as == 0 && bs == 0
+         if ai == as-1 && bi == bs-1
             compact(diff, w)
             compact(diff.reverse, w)
-            return diff 
+            return diff.map do |x| 
+               if x.w == 0 
+                  "...\n"
+               elsif x.op < 0 
+                  "- " + x.i.to_s + " " + b[x.i]
+               else 
+                 (x.op > 0 ? "+ " : "  ") + x.i.to_s + " " + a[x.i]
+               end
+#               ( (x.op > 0) ? "+" :(x.op < 0 ? "-" : " ")) + " " + x.w.to_s + " " + x.i.to_s + " " + x.line 
+            end.chunk{|x| x}.map(&:first)
          end
           
          rs1 = []
-         rs1 << Cand.new(price + 1, aa[0..as-1], bb[1..bs-1], diff + [ Res.new(+1, 666, "+ " + bb[0], 0)]) if bs > 0
-         rs1 << Cand.new(price + 1, aa[1..as-1], bb[0..bs-1], diff + [ Res.new(-1, 666, "- " + aa[0], 0)]) if as > 0
-         rs1 << Cand.new(price,     aa[1..as-1], bb[1..bs-1], diff + [ Res.new( 0, 666, "  " + aa[0], 0)]) if as > 0 && bs > 0 &&  aa[0] == bb[0]
+         rs1 << Cand.new(price+1,   ai, bi+1, diff + [ Res.new(+1, bi, "+ " + b[bi], 0)]) if bi < bs
+         rs1 << Cand.new(price+1, ai+1,   bi, diff + [ Res.new(-1, ai, "- " + a[ai], 0)]) if ai < as
+         rs1 << Cand.new(price,   ai+1, bi+1, diff + [ Res.new( 0, ai, "  " + a[ai], 0)]) if ai < as && bi < bs &&  a[ai] == b[bi]
          rs1
 
       end.flatten.sort{ |x,y| x.price <=> y.price }[0..20]
@@ -36,4 +46,6 @@ def diff(w,a,b)
 end
 
 #print diff(IO.readlines(ARGV[0]), IO.readlines(ARGV[1])).map(&:line).join("")
-print diff(4, IO.readlines(ARGV[0]), IO.readlines(ARGV[1])).map { |x| "[" + x.w.to_s + "] " + x.line }.join("")
+a, b = IO.readlines(ARGV[0]), IO.readlines(ARGV[1])
+#print diff(4, a, b).map { |x| "[" + x.w.to_s + "] " + x.line }.join("")
+print diff(4, a, b).join("")
