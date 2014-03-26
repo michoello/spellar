dd47b19dd612503e1b29ac121ce437af4845e462
michoello <midolini@midolini.redmond.corp.microsoft.com>
2014-03-25 18:38:32 -0700

    the brilliant diff is ready

--- diff/diff.rb
+++ diff/diff.rb
@@ -1,12 +1,10 @@
-Cand = Struct.new(:price, :a, :b, :diff)
-Res = Struct.new(:op, :i, :line, :w)
+Cand = Struct.new(:price, :ai, :bi, :diff)
+Res = Struct.new(:op, :i, :w)
 
 class Array
-   def compact(w)
-      rr = 0
-      self.each do |x| 
-         rr = x.op != 0 ? w : rr-1; 
-         x.w += rr unless rr < 0
+   def taint(w)
+      self.each_cons(2) do |x,y|
+         y.w += x.w - 1 if y.op == 0 && x.w > 0
       end
       self.reverse
    end
@@ -14,15 +12,14 @@ end
 
 # w is amount of lines surrounding modified parts, a and b are compared arrays
 def diff(w,a,b)
-   as, bs = a.size, b.size
-   rs = [ Cand.new(0, 0, 0, []) ]
+   as, bs, rs = a.size, b.size, [ Cand.new(0, 0, 0, []) ]
 
    until rs.empty? do
       rs = rs.map do |r|
          price, ai, bi, diff = r.values
 
          if ai == as-1 && bi == bs-1
-            return diff.compact(w).compact(w).map do |x| 
+            return diff.taint(w).taint(w).map do |x| 
                if x.w == 0 
                   "...\n"
                elsif x.op <= 0 
@@ -30,20 +27,18 @@ def diff(w,a,b)
                else 
                   "+ " + (x.i+1).to_s + " " + b[x.i]
                end
-            end.chunk{|x| x}.map(&:first)
+            end.chunk{|x| x}.map(&:first) # uniq adjacent
          end
           
          rs1 = []
-         rs1 << Cand.new(price+1,   ai, bi+1, diff + [ Res.new(+1, bi, "+ " + b[bi], 0)]) if bi < bs
-         rs1 << Cand.new(price+1, ai+1,   bi, diff + [ Res.new(-1, ai, "- " + a[ai], 0)]) if ai < as
-         rs1 << Cand.new(price,   ai+1, bi+1, diff + [ Res.new( 0, ai, "  " + a[ai], 0)]) if ai < as && bi < bs && a[ai] == b[bi]
+         rs1 << Cand.new(price+1,   ai, bi+1, diff + [Res.new(+1, bi, w)]) if bi < bs
+         rs1 << Cand.new(price+1, ai+1,   bi, diff + [Res.new(-1, ai, w)]) if ai < as
+         rs1 << Cand.new(price,   ai+1, bi+1, diff + [Res.new( 0, ai, 0)]) if ai < as && bi < bs && a[ai] == b[bi]
          rs1
 
-      end.flatten.sort{ |x,y| x.price <=> y.price }[0..20]
+      end.flatten.sort{|x,y| x.price <=> y.price}[0..20]
    end
 end
 
-#print diff(IO.readlines(ARGV[0]), IO.readlines(ARGV[1])).map(&:line).join("")
 a, b = IO.readlines(ARGV[0]), IO.readlines(ARGV[1])
-#print diff(4, a, b).map { |x| "[" + x.w.to_s + "] " + x.line }.join("")
 print diff(4, a, b).join("")
