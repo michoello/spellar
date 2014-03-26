a2444c9f7c7f60f9f198159dda0f39d80f8253ec
michoello <midolini@midolini.redmond.corp.microsoft.com>
2014-03-25 18:22:41 -0700

    almost brilliant

--- diff/diff.rb
+++ diff/diff.rb
@@ -1,12 +1,14 @@
 Cand = Struct.new(:price, :a, :b, :diff)
 Res = Struct.new(:op, :i, :line, :w)
 
-def compact(d, w)
-   rr = 0
-   d.each do |x| 
-      rr = x.op != 0 ? w : rr-1; 
-      rr = 0 if rr < 0; 
-      x.w += rr 
+class Array
+   def compact(w)
+      rr = 0
+      self.each do |x| 
+         rr = x.op != 0 ? w : rr-1; 
+         x.w += rr unless rr < 0
+      end
+      self.reverse
    end
 end
 
@@ -18,27 +20,23 @@ def diff(w,a,b)
    until rs.empty? do
       rs = rs.map do |r|
          price, ai, bi, diff = r.values
-#         as, bs = aa.size, bb.size
 
          if ai == as-1 && bi == bs-1
-            compact(diff, w)
-            compact(diff.reverse, w)
-            return diff.map do |x| 
+            return diff.compact(w).compact(w).map do |x| 
                if x.w == 0 
                   "...\n"
-               elsif x.op < 0 
-                  "- " + x.i.to_s + " " + b[x.i]
+               elsif x.op <= 0 
+                  (x.op ==0 ? "  " : "- ") + (x.i+1).to_s + " " + a[x.i]
                else 
-                 (x.op > 0 ? "+ " : "  ") + x.i.to_s + " " + a[x.i]
+                  "+ " + (x.i+1).to_s + " " + b[x.i]
                end
-#               ( (x.op > 0) ? "+" :(x.op < 0 ? "-" : " ")) + " " + x.w.to_s + " " + x.i.to_s + " " + x.line 
             end.chunk{|x| x}.map(&:first)
          end
           
          rs1 = []
          rs1 << Cand.new(price+1,   ai, bi+1, diff + [ Res.new(+1, bi, "+ " + b[bi], 0)]) if bi < bs
          rs1 << Cand.new(price+1, ai+1,   bi, diff + [ Res.new(-1, ai, "- " + a[ai], 0)]) if ai < as
-         rs1 << Cand.new(price,   ai+1, bi+1, diff + [ Res.new( 0, ai, "  " + a[ai], 0)]) if ai < as && bi < bs &&  a[ai] == b[bi]
+         rs1 << Cand.new(price,   ai+1, bi+1, diff + [ Res.new( 0, ai, "  " + a[ai], 0)]) if ai < as && bi < bs && a[ai] == b[bi]
          rs1
 
       end.flatten.sort{ |x,y| x.price <=> y.price }[0..20]
